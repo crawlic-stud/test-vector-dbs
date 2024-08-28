90456947e2176e2edf4eeebac230af4ccead78e7
crawlic-stud <nikitosik0726@gmail.com>
2024-08-28 11:37:48 +0300

    hmmm

--- src/qdrant_test.py
+++ src/qdrant_test.py
@@ -1,27 +1,24 @@
+import sys
 import time
-from qdrant_client.models import PointStruct
-import numpy as np
-from qdrant_client.models import Distance, VectorParams
+
 from qdrant_client import QdrantClient
 
 from pastas import get_pastas
 
-client = QdrantClient("/qdrant-db")
+# host with docker:
+# https://qdrant.tech/documentation/quickstart/#download-and-run
+client = QdrantClient("http://localhost:6333")
 
-texts = get_pastas()
-ids = list(range(len(texts)))
+# texts = get_pastas()
+# ids = list(range(len(texts)))
 
-client.add(
-    "my_collection",
-    texts
-)
+# client.add("my_collection", texts)
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    
+
     search_result = client.query(
-        collection_name="my_collection",
-        query_text="я гей"
+        collection_name="my_collection", query_text=sys.argv[1]
     )
-    print(f"done in {time.perf_counter - start:.2f} s")
-    print(search_result)
+    print(f"done in {time.perf_counter() - start:.2f} s")
+    print([f"{r.score:.2f}: {r.document}" for r in search_result])
